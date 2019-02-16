32987c7a725adb8fd93b2e7c96ea09be674a1703
Joe <development@inglenookdevelopment.com>
2019-02-16 21:23:58 +0000

    completed basic sample app

---
+++ sample/MaterialTextViewSample/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Views.InputMethods;
+using Xama.JTPorts.MaterialTextField;
+
+namespace MaterialTextFieldSample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private MaterialTextField materialTextView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            materialTextView = FindViewById<MaterialTextField>(Resource.Id.mtvInput);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            InputMethodManager imm = (InputMethodManager)GetSystemService(Context.InputMethodService);
+            imm.HideSoftInputFromWindow(materialTextView.WindowToken, 0);
+
+            return base.OnTouchEvent(e);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+	}
+}
+
